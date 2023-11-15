f19e79ad10fa58b8f254a40ad37ede86f8ee8347
errepeje <63585023+errepeje@users.noreply.github.com>
2023-11-15 13:21:59 +0100

    Find funcional de prueba

--- Anthem-Manifest_The-Smart-City/routes/test.js
+++ Anthem-Manifest_The-Smart-City/routes/test.js
@@ -6,8 +6,18 @@ var debug = require('debug')('SmartCity:server');
 
 const db = mongoose.connection;
 
-router.get('/', (req, res, next) => {
-  res.send('¡La conexión con MongoDB Atlas funciona correctamente!');
+/* GET all accidents */
+router.get('/', async (req, res) => {
+  try {
+    // Realiza la consulta para obtener todos los accidentes
+    const accidentes = await db.collection("CIUDAD_ACCIDENTALIDAD").find({}).toArray();
+        // Envía los resultados como respuesta
+        res.json(accidentes);
+        
+  } catch (error) {
+    // Maneja los errores y envía una respuesta de error
+    res.status(500).json({ error: 'Error al obtener los accidentes' });
+  }
 });
 
 module.exports = router;
